ed34df5674e1b935f888b9433729e5a6e04c3856
rocke <rocke@Manish>
2025-08-16 11:40:12 +0530

    Check bro

--- src/test/java/arceeTest/ToCreateArceeTransformerTest.java
+++ src/test/java/arceeTest/ToCreateArceeTransformerTest.java
@@ -5,6 +5,7 @@ public class ToCreateArceeTransformerTest {
 	public static void main(String[] args) {
 
 		System.out.println("Arcee Transformer Created");
+		System.out.println("BumbleB is now slave of Manish");
 
 	}
 
--- src/test/java/cliffJumperTest/ToCreateCliffJumperTransformerTest.java
+++ src/test/java/cliffJumperTest/ToCreateCliffJumperTransformerTest.java
@@ -5,6 +5,9 @@ public class ToCreateCliffJumperTransformerTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Cliff Jumper Created and Transformed Successfully");
+		System.out.println("Manish is about to jump from cliff");
+		System.out.println("Manish has commited Sucide");
+		
 
 	}
 
